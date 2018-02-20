17f108039bc76096d8d0a61d09350c86c67b7c6b
Matthew Leon <ml@matthewleon.com>
2018-02-20 18:06:44 -0500

    don't use Arbitrary in tests

--- test/Main.purs
+++ test/Main.purs
@@ -7,12 +7,12 @@ import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Random (RANDOM)
 import Data.ModularArithmetic (class Prime, Z, genZ, inverse)
-import Test.QuickCheck (class Arbitrary, Result, quickCheck, (<?>))
-import Test.QuickCheck.Laws.Data (checkCommutativeRing, checkEuclideanRing, checkField, checkRing, checkSemiring)
-import Type.Data.Nat (class Pos, D11)
-import Type.Proxy (Proxy(..))
+import Test.QuickCheck (Result, quickCheck, (<?>))
+import Test.QuickCheck.Gen (Gen)
+import Test.QuickCheck.Laws.Data (checkCommutativeRingGen, checkEuclideanRingGen, checkFieldGen, checkRingGen, checkSemiringGen)
+import Type.Data.Nat (D11)
 
-type Z11 = ArbitraryZ D11
+type Z11 = Z D11
 
 main
   :: forall eff
@@ -23,35 +23,21 @@ main
      | eff )
      Unit
 main = do
-  let p = Proxy :: Proxy Z11
+  let gen = genZ :: Gen Z11
 
-  quickCheck (inverses :: Z11 -> Result)
+  quickCheck $ (inverses :: Z11 -> Result) <$> gen
 
-  checkSemiring p
-  checkRing p
-  checkCommutativeRing p
-  checkEuclideanRing p
-  checkField p
+  checkSemiringGen gen
+  checkRingGen gen
+  checkCommutativeRingGen gen
+  checkEuclideanRingGen gen
+  checkFieldGen gen
 
-inverses :: forall m. (Prime m) => ArbitraryZ m -> Result
-inverses (ArbitraryZ x) = go <?> "x: " <> show x
+inverses :: forall m. Prime m => Z m -> Result
+inverses x = go <?> "x: " <> show x
   where
     go
       | x /= zero =
           x * inverse x == one
       | otherwise =
           true
-
-newtype ArbitraryZ m = ArbitraryZ (Z m)
-derive newtype instance eqZ :: Eq (ArbitraryZ m)
-derive newtype instance ordZ :: Ord (ArbitraryZ m)
-derive newtype instance showZ :: Show (ArbitraryZ m)
-derive newtype instance semiringZ :: Pos m => Semiring (ArbitraryZ m)
-derive newtype instance ringZ :: Pos m => Ring (ArbitraryZ m)
-derive newtype instance commutativeRingZ
-  :: Pos m => CommutativeRing (ArbitraryZ m)
-derive newtype instance euclideanRingZ
-  :: Prime m => EuclideanRing (ArbitraryZ m)
-derive newtype instance fieldZ :: Prime m => Field (ArbitraryZ m)
-instance arbitraryZ :: Pos m => Arbitrary (ArbitraryZ m) where
-  arbitrary = ArbitraryZ <$> genZ
